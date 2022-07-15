655f71ea30562dcaa2936521312c2bd44686135a
Fedor <example@gmail.com>
2022-07-15 15:30:35 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,27 @@
+"srtict";
+
+const obj = {
+  name: "Petir",
+  arg: 22,
+  id: 125
+} ;
+console.log(obj.id);
+
+
+let arr = [1, 2, 3, "html", "css" ];
+console.log(arr[2]);
+
+const arrr = prompt("вам есть 18", "");
+console.log(arrr); 
+
+const answer = confirm("dfsakdfjaf");
+
+const hit = [];
+hit[0] = prompt('сколько вам лет');
+hit[1] = prompt('как вас зовут');
+hit[2] = prompt('Ваша фамилия');
+
+document.write(typeof(hit));
+
+const jon = 'fedor';
+console.log(`Привет,${jon}`);
